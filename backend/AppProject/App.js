83e95ab29a9ccf9ceffcb56c72ebc533fb651a41
Boris <borisculjak@gmail.com>
2022-08-30 13:56:05 +0200

    Radi log in i log out

--- backend/AppProject/App.js
+++ backend/AppProject/App.js
@@ -10,17 +10,30 @@ import Login from './components/Login';
 import Contants from 'expo-constants';
 
 import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Button } from 'react-native-paper';
 
 const Stack = createStackNavigator()
 
 function App() { //Imamo funkcionalne(rcf) i klasne komponente(rce), ovo je funkcionalna
+  const navigation = useNavigation()
   return (
     <View style={styles.container}>
       <Stack.Navigator>
         <Stack.Screen name = "Login" component={Login}/>
-        <Stack.Screen name = "Home" component={Home}/>
+        <Stack.Screen 
+          name = "Home" 
+          component={Home} 
+          options = {{
+            headerRight: () => (
+              <Button
+                onPress = {() => {AsyncStorage.clear(); navigation.navigate('Login')}}
+              >Logout</Button>
+            ),
+          }}
+        />
         <Stack.Screen name = "Create" component={Create}/>
         <Stack.Screen name = "Details" component={Details}/>
         <Stack.Screen name = "Edit" component={Edit}/>
--- backend/AppProject/components/Login.js
+++ backend/AppProject/components/Login.js
@@ -1,71 +1,82 @@
-import React, {useState, useEffect, useContext} from 'react'
-import {View, Text, StyleSheet} from 'react-native'
-import {Button, TextInput} from 'react-native-paper'; 
+import React, { useState, useEffect, useContext } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { Button, TextInput } from 'react-native-paper';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
-export default function Login() { 
+export default function Login(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("")
-  const token = AsyncStorage.getItem("token")
+  const [rerender, setRerender] = useState(false)
 
-  const Login = () => {
+
+  const handleClick = () => {
     fetch('http://192.168.56.1:3000/token', {
-      method:'POST',
+      method: 'POST',
       headers: {
-        'Content-Type' : 'application/json'
+        'Content-Type': 'application/json'
       },
-      body: JSON.stringify({email:email, password:password})
+      body: JSON.stringify({ email: email, password: password })
     })
-    .then(resp => {
-      if(resp.status == 200) return resp.json();
-      else alert("There has been some error")
-    })
-    .then(data => {
-      console.log("this came from the backend", data)
-      AsyncStorage.setItem("token", data.access_token)
-    })
-    .catch(error=>console.log(error))
+      .then(resp => {
+        if (resp.status == 200) return resp.json();
+        else alert("Pogresan user ili password")
+      })
+      .then(data => {
+        console.log("this came from the backend", data)
+        AsyncStorage.setItem("token", data.access_token)
+        setRerender(!rerender)
+      })
+      .catch(error => console.log(error))
   }
+  // User Authentication
+  (async () => {
+    try {
+      const token = await AsyncStorage.getItem('token')
+      console.log("OVO JE TOKENCINA: " + token)
+      if (token && token !== 'undefined' && token !== '') {
+          console.log('User logged in.')
+          props.navigation.navigate('Home')
+        
+      }
+    } catch (e) {
+      console.log('Failed to login user.')
+    }
+  })()
 
-  console.log(token)
   return (
     <View>
-      {(token && token!="[object Object]" && token!=undefined) ? <Text>You are logged in with this token</Text> + token :
-        <View>
-          <TextInput 
-            label = "Email"
-            mode = "outlined"
-            onChangeText={email => setEmail(email)}
-          />
+      <TextInput
+        label="Email"
+        mode="outlined"
+        onChangeText={email => setEmail(email)}
+      />
 
-          <TextInput
-            label = "Password"
-            mode = "outlined"
-            onChangeText={password => setPassword(password)}
-          />
+      <TextInput
+        label="Password"
+        mode="outlined"
+        onChangeText={password => setPassword(password)}
+      />
 
-          <Button
-            style = {styles.buttonStyle}
-            mode = "contained"
-            icon = "check"
-            onPress = {()=>Login()}
-          />
-        </View>
-    }
-   </View>
+      <Button
+        style={styles.buttonStyle}
+        mode="contained"
+        icon="check"
+        onPress={() => handleClick()}
+      />
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
-  cardStyle:{
-      flex: 1,
-      margin:10,
-      padding:10
+  cardStyle: {
+    flex: 1,
+    margin: 10,
+    padding: 10
   },
-  buttonStyle:{
-    marginTop:10,
-    margin:10
+  buttonStyle: {
+    marginTop: 10,
+    margin: 10
   }
 })
 
