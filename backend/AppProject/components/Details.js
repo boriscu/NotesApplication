aa9bef404e7995a3d1a6ca0c28956ab304756689
Boris <borisculjak@gmail.com>
2022-10-19 11:33:56 +0200

    Dodao auto-refresh
    Ne radi delete

--- backend/AppProject/components/Details.js
+++ backend/AppProject/components/Details.js
@@ -1,68 +1,61 @@
-import React from 'react'
-import {View, ScrollView, Text, StyleSheet} from 'react-native'
-import { Button } from 'react-native-paper';
-//Dugme u poseban view jer je view kao labela 
+import React from "react";
+import { View, ScrollView, Text, StyleSheet } from "react-native";
+import { Button } from "react-native-paper";
+//Dugme u poseban view jer je view kao labela
 export default function Details(props) {
   const data = props.route.params.data;
 
   const deleteData = (data) => {
     fetch(`http:///192.168.56.1:3000/delete/${data.id}/`, {
-      method:'DELETE',
+      method: "DELETE",
       headers: {
-        'Content-Type' : 'application/json'
-      }
-
+        "Content-Type": "application/json",
+      },
     })
-    .then(data => {
-      props.navigation.navigate("Home")
-    })
-    .catch(error => console.log(error))
-  }
+      .then((data) => {
+        props.navigation.navigate("Home");
+      })
+      .catch((error) => console.log(error));
+  };
   return (
     <ScrollView>
-        <View style = {styles.viewStyle}>
-          <Text style = {{fontSize:25}}>
-            {data.title}
-          </Text>
-          <Text style = {{fontSize:20, marginTop:10}}>
-            {data.body}
-          </Text>
-          <Text style = {{fontSize:20}}>
-            {data.uidate}
-          </Text>
-          <Text style = {{fontSize:10}}>
-            {data.date}
-          </Text>
-          
-          <View style = {styles.btnStyle}>
-            <Button
-              icon = "update"
-              mode = "contained"
-              onPress={()=>props.navigation.navigate("Edit", {data:data})}
-            >Edit</Button>
+      <View style={styles.viewStyle}>
+        <Text style={{ fontSize: 25 }}>{data.title}</Text>
+        <Text style={{ fontSize: 20, marginTop: 10 }}>{data.body}</Text>
+        <Text style={{ fontSize: 20 }}>{data.uidate}</Text>
+        <Text style={{ fontSize: 10 }}>{data.date}</Text>
 
-            <Button
-              icon = "delete"
-              mode = "contained"
-              onPress={()=>deleteData(data)}
-            >Delete</Button>
-          
-          </View>
+        <View style={styles.btnStyle}>
+          <Button
+            icon="update"
+            mode="contained"
+            onPress={() => props.navigation.navigate("Edit", { data: data })}
+          >
+            Edit
+          </Button>
+
+          <Button
+            icon="delete"
+            mode="contained"
+            onPress={() => deleteData(data)}
+          >
+            Delete
+          </Button>
         </View>
+      </View>
     </ScrollView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   viewStyle: {
-    padding:10,
-    margin:10
+    padding: 10,
+    margin: 10,
   },
   btnStyle: {
-    flexDirection:"row",
-    justifyContent:"space-around",
-    margin:15,
-    padding:10
-
-  }
-})
+    flexDirection: "row",
+    justifyContent: "space-around",
+    margin: 15,
+    padding: 10,
+  },
+});
